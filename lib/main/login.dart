afcb61b430ebdc549a431fb26307229231f3f607
Parada T. <Parada.t@praram-3.com>
2026-08-06 09:46:56 +0700

    test

--- lib/main/login.dart
+++ lib/main/login.dart
@@ -180,7 +180,7 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                     const SizedBox(height: 16),
                     Text(
-                      'เข้าสู่ระบบ',
+                      'เข้าสู่ระบบจ้า',
                       style: AppTextStyles.text(
                         fontSize: 22,
                         fontWeight: FontWeight.w700,
